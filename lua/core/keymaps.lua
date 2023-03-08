a9627b8b540db64bdddf7230c6220f4710e9f712
Anton Kolesov <kolesov.anton.s@gmail.com>
2023-03-09 00:53:16 +0300

    Disabled arrows

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -5,3 +5,11 @@ vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+
+-- Disable arrows
+for _, mode in pairs({ 'n', 'i', 'v', 'x' }) do
+    for _, key in pairs({ '<Up>', '<Down>', '<Left>', '<Right>' }) do
+        vim.keymap.set(mode, key, '<nop>')
+    end
+end
